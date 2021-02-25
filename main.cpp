ce2883da7159a99689d74f9a1550d5b1de1621e7
Kevin Mallari <kevin.mallari@gmail.com>
2021-02-25 12:20:56 +0800

    Added reading from file

--- main.cpp
+++ main.cpp
@@ -1,10 +1,39 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <sstream>
 using namespace std;
+
+//will replace isDboule function with sir lui's code
+bool isDouble(string s, double &value)
+{
+  double tempValue;
+  if ((istringstream(s) >> tempValue).eof())
+  {
+    value = tempValue;
+    cout << "Your string \"" << value << "\" is a valid double.";
+    return true;
+  }
+  else
+  {
+    cout << "Your string \"" << s << "\" is NOT a valid double.";
+    return false;
+  }
+}
 
 int main()
 {
-  cout << "Hello world" << endl;
+  string fileName1, fileName2, signalText1, signalText2;
+  cin >> fileName1;
+  fstream signalFile1(fileName1);
+  if (signalFile1.is_open())
+  {
+    while (getline (signalFile1, signalText1))
+    {
+      cout << signalText1 << endl;
+    }
+    signalFile1.close();
+  }
   return 0;
 }
