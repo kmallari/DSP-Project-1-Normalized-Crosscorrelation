1c8fef0dffb67cab20d8c6aad3c73e83b3ce2b6b
Decaffeinated-Usagi <luke.lao@obf.ateneo.edu>
2021-03-02 16:17:29 +0800

    Tried compiling

--- main.cpp
+++ main.cpp
@@ -3,125 +3,73 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
+#include <algorithm>
+#include <stdlib.h>
 #include "isDouble.h"
 #include "isInt.h"
 #include "extractSignals.h"
+#include "FileWrite.h"
+#include "PrintData.h"
+#include "getMean.h"
+#include "subtractAverage.h"
+#include "NormalCoeff.h"
+#include "Normalize.h"
+#include "Adjust.h"
+#include "getCrossCorrelation.h"
+
 using namespace std;
-
-/* bool isInt(string s, int &value)
-{
-  int tempValue;
-  if ((istringstream(s) >> tempValue).eof())
-  {
-    value = tempValue;
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-}*/
-
-//will replace isDboule function with sir lui's code
-/*bool isDouble(string s, double &value)
-{
-  double tempValue;
-  if ((istringstream(s) >> tempValue).eof())
-  {
-    value = tempValue;
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-} */
-
-vector<double> extractSignals(string fileName, int &signalsIndex)
-{
-  string signalText, tempString;
-  stringstream ss, ss2;
-  int lineNumber = 0;
-  double tempDouble;
-  vector<double> signals;
-
-  fstream signalFile(fileName);
-
-  if (signalFile.is_open())
-  {
-    while (getline(signalFile, signalText))
-    {
-      if (lineNumber == 0)
-      {
-        ss << signalText;
-        ss >> tempString;
-        if (!isInt(tempString, signalsIndex))
-        {
-          cout << "THIS RUNS" << endl;
-          if (isDouble(tempString, tempDouble))
-          {
-            signals.push_back(tempDouble);
-          }
-          signalsIndex = 0;
-        }
-        else 
-        {
-          ss >> tempString;
-          if (isDouble(tempString, tempDouble))
-          {
-            signals.push_back(tempDouble);
-          }
-          ss.clear();
-        }
-        lineNumber++;
-      }
-      else
-      {
-        ss2 << signalText;
-        ss2 >> tempString;
-        ss2.clear();
-
-        if (isDouble(tempString, tempDouble))
-        {
-          signals.push_back(tempDouble);
-        }
-        tempString = "";
-      }
-    }
-    signalFile.close();
-  }
-  return signals;
-}
 
 int main()
 {
-  string fileName1 = "signal-1.txt", fileName2 = "signal-2.txt";
-  int signalsIndex1, signalsIndex2;
-  vector<double> firstSignals, secondSignals;
+  string fileName1, fileName2;
+  int signalsIndex1, signalsIndex2, signal1LastIndex, signal2LastIndex;
+  int lastL, firstL, type;
+  vector<double> firstSignals, secondSignals, temp, r_xy;
 
-  // cout << "Enter file name for the first signals: ";
-  // cin >> fileName1;
+  cout << "Enter file name for the first signals: ";
+  getline(cin, fileName1);
   
-  firstSignals = extractSignals(fileName1, signalsIndex1);
+  firstSignals = extractSignals(fileName1.c_str(), signalsIndex1);
   cout << "Index 1: " << signalsIndex1 << endl;
   for (int i = 0; i < firstSignals.size(); i++)
   {
     cout << firstSignals.at(i) << endl;
   }
 
-  // cout << "Enter file name for the second signals: ";
-  // cin >> fileName1;
+  cout << "Enter file name for the second signals: ";
+  getline(cin, fileName2);
 
-  secondSignals = extractSignals(fileName2, signalsIndex2);
-  
+  secondSignals = extractSignals(fileName2.c_str(), signalsIndex2);
   cout << "Index 2: " << signalsIndex2 << endl;
   for (int i = 0; i < secondSignals.size(); i++)
   {
     cout << secondSignals.at(i) << endl;
   }
+  
+  signal1LastIndex=signalsIndex1+firstSignals.size()-1;
+  signal2LastIndex=signalsIndex2+secondSignals.size()-1; //is this needed?
+  lastL=signal1LastIndex-signalsIndex2;
+  firstL=signalsIndex1-signal2LastIndex;
 
-  int x = 4.5;
-  cout << "PRINTING INT " << x << endl;
+  if(signalsIndex1<signalsIndex2)
+  {
+    type = 1; //inserts element to start of array via insert begin
+  }
+  else if(signalsIndex1>signalsIndex2)
+  {
+    type = 2; //inserts element to end of array via pushback
+  }
+
+  subtractAverage(firstSignals, getMean(firstSignals));
+  subtractAverage(secondSignals, getMean(secondSignals));
+
+  temp = secondSignals;
+
+  Adjust(temp, firstSignals.size());
+  getCrossCorrelation(temp, r_xy, firstSignals, firstL, lastL, type);
+  Normalize(r_xy, NormalCoeff(firstSignals,secondSignals));
+  PrintData(r_xy);
+  FileWrite(r_xy, firstL);
 
   return 0;
 }
